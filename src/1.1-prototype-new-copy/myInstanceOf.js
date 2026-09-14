d1d942d03aa274d87bdc3b69cc2f68451411886f
Tiestoincontrol <tiestoincontrol@gmail.com>
2026-09-14 18:47:25 +0300

    myInstanceOf second shot: .prototype has only functions, mixed up prototype and target in implementation

--- src/1.1-prototype-new-copy/myInstanceOf.js
+++ src/1.1-prototype-new-copy/myInstanceOf.js
@@ -1,15 +1,29 @@
 function myInstanceOf(obj, ctor) {
-  console.log('111', Object.getPrototypeOf(obj));
-  console.log('222', Object.getPrototypeOf(new ctor()));
+  const prototype = ctor.prototype;
+  let target = Object.getPrototypeOf(obj);
+  while (prototype !== null) {
+    if (prototype === target) return true;
+    prototype = Object.getPrototypeOf(prototype);
+  }
+  return false;
 }
-function Cat() {}
+// function Animal() {}
 
-function Dog() {}
+// function Cat(...args) {
+//   Animal.call(this, ...args);
+// }
 
-const cat = new Cat();
-const tiger = new Cat();
-const dog = new Dog();
+// Cat.prototype = Object.create(Animal.prototype);
+// Cat.prototype.constructor = Cat;
 
-console.log(cat instanceof Cat);
-// console.log(Object.getPrototypeOf(Cat));
-console.log(myInstanceOf(tiger, Cat));
+// function Dog() {}
+
+// const cat = new Cat();
+// const tiger = new Cat();
+// const dog = new Dog();
+
+// console.log(cat instanceof Cat);
+// console.log(myInstanceOf(tiger, Cat));
+// console.log(myInstanceOf(dog, Cat));
+// console.log(myInstanceOf(cat, Animal));
+// console.log(myInstanceOf(dog, Animal));
