9e749d82f56a91ae3e6e0635165009fb521812f3
Tiestoincontrol <tiestoincontrol@gmail.com>
2026-09-14 18:48:14 +0300

    final implementation of myInstanceOf

--- src/1.1-prototype-new-copy/myInstanceOf.js
+++ src/1.1-prototype-new-copy/myInstanceOf.js
@@ -1,29 +1,31 @@
 function myInstanceOf(obj, ctor) {
-  const prototype = ctor.prototype;
-  let target = Object.getPrototypeOf(obj);
+  if (obj === null || (typeof obj !== 'function' && typeof obj !== 'object'))
+    return false;
+  const target = ctor.prototype;
+  let prototype = Object.getPrototypeOf(obj);
   while (prototype !== null) {
     if (prototype === target) return true;
     prototype = Object.getPrototypeOf(prototype);
   }
   return false;
 }
-// function Animal() {}
+function Animal() {}
 
-// function Cat(...args) {
-//   Animal.call(this, ...args);
-// }
+function Cat(...args) {
+  Animal.call(this, ...args);
+}
 
-// Cat.prototype = Object.create(Animal.prototype);
-// Cat.prototype.constructor = Cat;
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat;
 
-// function Dog() {}
+function Dog() {}
 
-// const cat = new Cat();
-// const tiger = new Cat();
-// const dog = new Dog();
+const cat = new Cat();
+const tiger = new Cat();
+const dog = new Dog();
 
-// console.log(cat instanceof Cat);
-// console.log(myInstanceOf(tiger, Cat));
-// console.log(myInstanceOf(dog, Cat));
-// console.log(myInstanceOf(cat, Animal));
-// console.log(myInstanceOf(dog, Animal));
+console.log(cat instanceof Cat);
+console.log(myInstanceOf(tiger, Cat));
+console.log(myInstanceOf(dog, Cat));
+console.log(myInstanceOf(cat, Animal));
+console.log(myInstanceOf(dog, Animal));
